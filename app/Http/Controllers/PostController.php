fbc0792428bea1978974fddf310c82d80d16bdb3
Mehorab Hossain <mehorab@codevioso.com>
2025-04-08 19:08:04 +0600

    Controller update

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StorePostRequest;
+use App\Models\Category;
+use App\Models\Post;
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+        $posts = Post::with('catergories', 'user')->latest()->get();
+        return view('posts.index', compact('posts'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+        $categories = Category::all();
+        return view('posts.create', compact('categories'));
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StorePostRequest $request)
+    {
+        //
+        $post = auth()->user()->posts()->create($request->only(['title', 'content', 'status']));
+        $post->categories()->attach($request->categories);
+        return redirect()->route('posts.index')->with('success', 'Post created successfully.');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Post $post)
+    {
+        //
+        return view('posts.show', compact('post'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Post $post)
+    {
+        //
+        $this->authorize('update', $post);
+        $categories = Category::all();
+        return view('posts.edit', compact('post', 'categories'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(StorePostRequest $request, Post $post)
+    {
+        //
+        $this->authorize('update', $post);
+        $post->update($request->only(['title', 'content', 'status']));
+        $post->categories()->sync($request->categories);
+        return redirect()->route('posts.index')->with('success', 'Post updated successfully.');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Post $post)
+    {
+        //
+        $this->authorize('delete', $post);
+        $post->delete();
+        return redirect()->route('posts.index')->with('success', 'Post deleted successfully.');
+    }
+}
---
+++ app/Http/Requests/StorePostRequest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StorePostRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return false;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'title' => 'required|max:255',
+            'content' => 'required',
+            'status' => 'required|in:draft,published',
+            'categories' => 'required|array|min:1',
+            'categories.*' => 'integer|exists:categories,id'
+            //
+        ];
+    }
+}
